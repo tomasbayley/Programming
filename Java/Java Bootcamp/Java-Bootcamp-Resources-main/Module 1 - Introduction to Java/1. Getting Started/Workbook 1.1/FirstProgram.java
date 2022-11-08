677335c9203411897ac2970d1789af4af02159f1
tbayley <tomibayley@hotmail.com>
2022-11-08 13:18:14 -0300

    3: Section 2

---
+++ Java/Java Bootcamp/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Workbook 1.1/FirstProgram.java
@@ -0,0 +1,11 @@
+// I'm a comment. I have no impact on your code.
+// Instructions for this workbook are on Learn the Part (See the Udemy Video: Workbook 1.1 to access the link).
+class FirstProgram{
+
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Phyton.");
+        System.out.println("I spilled java all over my paper.");
+        System.out.println("My dog ate my java");
+    }
+}
